d7b298dfb897d40d6753c920c3209432cdcb188f
Lucas Bueno Cesario <lbc92@hotmail.com>
2021-05-13 13:24:20 -0300

    Typewriter component; finished Home Page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,20 +1,22 @@
 import { Button, Row, Col } from 'react-bootstrap';
 import Link from 'next/link';
 import { FaCrosshairs } from 'react-icons/fa'; 
+import Typewriter from '../components/Typewriter';
 
 export default function Home() {
   return (
-    <Row className="mt-8">
+    <Row className="mt-8 justify-content-center">
       <Col md={7} xs={12} className="text-center">
-        <h1>
-          Comida saudável e gostosa direto na sua casa
-        </h1>
+        <h1 className='fw-bolder text-custom-gray-darker mb-5 lh-base display-5'>
+          <Typewriter text='Comida saudável e gostosa direto na sua casa'/>
+        </h1>      
+        <Link href='/restaurants'>
+          <Button variant='custom-red' size='lg' className='text-white'>
+            <FaCrosshairs className='pb-1'/>
+            <span className='px-2 fw-bolder'>ENCONTRAR AGORA</span>
+          </Button>
+        </Link>
       </Col>
-      <Link href='/restaurants'>
-        <Button variant='custom-red' size='lg' className='text-white'>
-          <FaCrosshairs/>
-        </Button>
-      </Link>
     </Row>
   )
 }
