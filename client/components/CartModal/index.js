e5839016a6f912d857161c8cd29556881c11c5a5
Lucas Bueno Cesario <lbc92@hotmail.com>
2021-05-14 16:35:57 -0300

    Cart created

---
+++ client/components/CartModal/index.js
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+import { Modal, Row, Col, Form, Button } from 'react-bootstrap';
+import Cart from '../Cart';
+
+import { useRecoilState } from 'recoil';
+import cartState from '../../store/atoms/cartAtom';
+
+export default function CartModal(props) {
+    const [cart] = useRecoilState(cartState);
+
+    return (
+        <Modal 
+            show={props.show}
+            size='sm'
+            aria-labelledby='contained-modal-title-vcenter'
+            centered
+            keyboard={false}
+            onHide={() => props.onHide()}
+        >
+            <Modal.Header>
+                <h5 className='fw-bold mt-2'>Carrinho</h5>
+            </Modal.Header>
+            <Modal.Body>
+                <Cart
+                    show={props.show}
+                />
+                {cart.products.length > 0 && (
+                    <div className='text-center pt-2'>
+                        <Link href='/orders/new'>
+                            <Button
+                                variant='custom-red'
+                                className='text-white'
+                            >
+                                Finalizar pedido
+                            </Button>
+                        </Link>
+                    </div>
+                )}
+            </Modal.Body>
+        </Modal>
+    )
+}
