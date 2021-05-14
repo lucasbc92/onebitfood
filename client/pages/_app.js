f9d4c1eb249424b9ee18fa157e626f0cd1a8e487
Lucas Bueno Cesario <lbc92@hotmail.com>
2021-05-14 13:30:34 -0300

    Recoil installed; address atom configured

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import '../styles/globals.scss';
 import Header from '../components/Header';
 import { Container } from 'react-bootstrap';
+import { RecoilRoot } from 'recoil';;
+
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -13,7 +15,9 @@ function MyApp({ Component, pageProps }) {
       <main>
         <Header />
         <Container className='mt-6'>
-          <Component {...pageProps} />
+          <RecoilRoot>
+            <Component {...pageProps} />
+          </RecoilRoot>
         </Container>        
       </main>
     </>
