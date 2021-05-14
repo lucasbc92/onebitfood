ec2778d53abf1a9645d5d31bbdfb16ba5a82e445
Lucas Bueno Cesario <lbc92@hotmail.com>
2021-05-14 18:55:10 -0300

    SSG in Restaurant detail page

--- client/components/ListRestaurants/index.js
+++ client/components/ListRestaurants/index.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Container, Row, Col, Spinner, Alert }  from 'react-bootstrap';
+import { Row, Col, Spinner, Alert } from 'react-bootstrap';
+import Restaurant from './Restaurant';
 import getRestaurants from '../../services/getRestaurants';
-import Restaurant from '../ListRestaurants/Restaurant';
-
 
 export default function ListRestaurants() {
-    const { restaurants, isLoading, isError } = getRestaurants();
-    
-    function renderContent() {
-        if(isError) {
-            return <Col><Alert variant='custom-red'>Erro ao carregar</Alert></Col>
-        } else if(isLoading) {
-            return <Col><Spinner animation='border'></Spinner></Col>
-        } else if(restaurants.length == 0) {
-            return <Col>Nenhum restaurante disponível</Col>
-        } else {
-            return restaurants.map((restaurant) => <Restaurant {...restaurant} key={restaurant.id}/>)
-        }
-    }   
+  const { restaurants, isLoading, isError } = getRestaurants();
 
-    return (
-        <div className='mt-5'>
-            <h3 className='fw-bold'>Restaurantes</h3>
-            <Row>
-                {renderContent()}
-            </Row>
-       </div>
-    )
+  function renderContent() {
+    if(isError)
+      return <Col><Alert variant='custom-red'>Erro ao carregar</Alert></Col>
+    else if(isLoading)
+      return <Col><Spinner animation='border'/></Col>
+    else if(restaurants.lenght == 0)
+      return <Col>Nenhum restaurante disponível ainda...</Col>
+    else
+      return restaurants.map((restaurant, i) => <Restaurant {...restaurant} key={i}/>)
+  }
+
+  return(
+    <div className='mt-5'>
+      <h3 className='fw-bold'>Restaurantes</h3>
+      <Row>
+        {renderContent()}
+      </Row>
+    </div>
+  )
 }
